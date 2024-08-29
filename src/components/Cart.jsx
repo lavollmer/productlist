faecab4a7894ceb643078484f9e5e30562d5b441
Laura <laura.e.vollmer@gmail.com>
2024-08-29 14:52:31 -0500

    added cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Cart = () => {
   return (
-    <div>Cart</div>
-  )
-}
+    <div className="bg-white rounded-lg p-6">
+      <div>
+        <h1 className="font-redhat text-red font-bold text-xl">Your Cart</h1>
+      </div>
+      <div></div>
+    </div>
+  );
+};
 
-export default Cart
+export default Cart;
