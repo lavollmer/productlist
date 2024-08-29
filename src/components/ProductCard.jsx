8c1a3d6a549bf1e1b4df78d78dca19908ae472eb
Laura <laura.e.vollmer@gmail.com>
2024-08-29 15:16:19 -0500

    styling the productlist card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -6,16 +6,18 @@ import CartIcon from "../assets/illustration-empty-cart.svg";
 
 const ProductCard = ({ title, price, category, picture, pictureDesc }) => {
   return (
-    <div className="font-redhat bg-rose-50">
-      <div>
-        <img src={picture} alt={pictureDesc} className="rounded-lg" />
-        <div className="flex flex-row items-center justify-center py-2 px-4 space-x-2 text-red bg-white rounded-full">
-          <img src={AddToCart} alt="Add to Cart" />
-          <button className="font-bold">Add to Cart</button>
+    <div className="flex flex-col font-redhat bg-rose-50">
+      <div className="relative">
+        <img src={picture} alt={pictureDesc} className="rounded-lg h-80 w-80" />
+        <div className="flex flex-col justify-center items-center">
+          <button className="absolute z-50 flex flex-row items-center justify-center py-3 px-6 space-x-2 bg-white rounded-full border border-rose-400">
+            <img src={AddToCart} alt="Add to Cart" />
+            <p className="font-bold text-rose-500">Add to Cart</p>
+          </button>
         </div>
       </div>
       <div className="flex flex-col justify-start bg-rose-50">
-        <p className="text-md">{category}</p>
+        <p className="text-md text-rose-500">{category}</p>
         <h1 className="font-bold text-lg">{title}</h1>
         <p className="text-red font-bold">{price}</p>
       </div>
