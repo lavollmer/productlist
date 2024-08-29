d7a671bc136146683a59ce43703246672c09d467
Laura <laura.e.vollmer@gmail.com>
2024-08-29 14:35:29 -0500

    added assets, colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        red: 'hsl(14, 86%, 42%)',
+        green: 'hsl(159, 69%, 38%)',
+        'rose-50': 'hsl(20, 50%, 98%)',
+        'rose-100': 'hsl(13, 31%, 94%)',
+        'rose-300': 'hsl(14, 25%, 72%)',
+        'rose-400': 'hsl(7, 20%, 60%)',
+        'rose-500': 'hsl(12, 20%, 44%)',
+        'rose-900': 'hsl(14, 65%, 9%)',
+      },
+      fontFamily: {
+        'redhat': ['redhat', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
