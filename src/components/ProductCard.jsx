3d47e73583d1e232ba61ce0e16f19d6713f5ece8
Laura <laura.e.vollmer@gmail.com>
2024-08-29 15:18:14 -0500

    styling the product list

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -8,7 +8,7 @@ const ProductCard = ({ title, price, category, picture, pictureDesc }) => {
   return (
     <div className="flex flex-col font-redhat bg-rose-50">
       <div className="relative">
-        <img src={picture} alt={pictureDesc} className="rounded-lg h-80 w-80" />
+        <img src={picture} alt={pictureDesc} className="rounded-lg h-50 w-60" />
         <div className="flex flex-col justify-center items-center">
           <button className="absolute z-50 flex flex-row items-center justify-center py-3 px-6 space-x-2 bg-white rounded-full border border-rose-400">
             <img src={AddToCart} alt="Add to Cart" />
@@ -16,10 +16,10 @@ const ProductCard = ({ title, price, category, picture, pictureDesc }) => {
           </button>
         </div>
       </div>
-      <div className="flex flex-col justify-start bg-rose-50">
-        <p className="text-md text-rose-500">{category}</p>
-        <h1 className="font-bold text-lg">{title}</h1>
-        <p className="text-red font-bold">{price}</p>
+      <div className="flex flex-col justify-start bg-rose-50 pt-10">
+        <p className="text-sm text-rose-500">{category}</p>
+        <h1 className="font-bold text-md">{title}</h1>
+        <p className="text-rose-800 font-bold">{price}</p>
       </div>
     </div>
   );
