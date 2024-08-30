2484738e50cd7a972cff1ab94675e42ed3e8887e
Laura <laura.e.vollmer@gmail.com>
2024-08-30 13:21:58 -0500

    updating functionality of the add to cart button with plus and minus

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import emptyCart from "../assets/illustration-empty-cart.svg";
 
-const Cart = () => {
+const Cart = ({quantity}) => {
   return (
     <div className="bg-white rounded-lg p-10">
       <div>
         <h1 className="font-redhat text-red font-bold text-xl">Your Cart</h1>
+        <h1 className="font-redhat text-red font-bold text-xl">{quantity}</h1>
       </div>
       <div>
         <img src={emptyCart} alt="Empty Cart" className="w-40 h-40" />
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,18 +3,65 @@ import AddToCart from "../assets/icon-add-to-cart.svg";
 import IncreaseIcon from "../assets/icon-increment-quantity.svg";
 import DecreaseIcon from "../assets/icon-decrement-quantity.svg";
 import CartIcon from "../assets/illustration-empty-cart.svg";
+import { useState } from "react";
 
-const ProductCard = ({ title, price, category, picture, pictureDesc }) => {
+const ProductCard = ({
+  title,
+  price,
+  category,
+  picture,
+  pictureDesc,
+  quantity,
+}) => {
+  //useState to hold the dynamic number of how many items are in the cart
+  const [quantity, setQuantity] = useState(0);
+
+  //if clicks the add to cart button for the first time --> always increases quantity by one
+  const handleQuantity = () => {
+    setQuantity(quantity + 1);
+  };
+
+  //if clicks the increase button --> increases quantity by one
+  const handleIncrease = () => {
+    setQuantity(quantity + 1);
+  };
+
+  //if clicks the decrease button --> decreases quantity by one
+  const handleDecrease = () => {
+    if (quantity > 0) {
+      setQuantity(quantity - 1);
+    }
+  };
+
   return (
     <div className="flex flex-col font-redhat bg-rose-50">
       <div className="relative">
         <img src={picture} alt={pictureDesc} className="rounded-lg h-50 w-60" />
         <div className="flex flex-col justify-center items-center">
-          <button className="absolute z-50 flex flex-row items-center justify-center py-2 px-6 space-x-2 bg-white rounded-full border border-rose-400">
+          <button
+            onClick={handleQuantity}
+            className="absolute z-50 flex flex-row items-center justify-center py-2 px-6 space-x-2 bg-white rounded-full border border-rose-400"
+          >
             <img src={AddToCart} alt="Add to Cart" />
             <p className="font-bold text-rose-500">Add to Cart</p>
           </button>
         </div>
+        {/* if quantity is greater than 0, show the quantity */}
+        {quantity > 0 && (
+          <div className="absolute z-50 flex flex-row items-center justify-center py-2 px-6 space-x-2 bg-white rounded-full border border-rose-400">
+            <img
+              onClick={handleIncrease}
+              src={DecreaseIcon}
+              alt="Decrease Quantity"
+            />
+            <p className="font-bold text-rose-500">{quantity}</p>
+            <img
+              onClick={handleDecrease}
+              src={IncreaseIcon}
+              alt="Increase Quantity"
+            />
+          </div>
+        )}
       </div>
       <div className="flex flex-col justify-start bg-rose-50 pt-10">
         <p className="text-sm text-rose-500">{category}</p>
