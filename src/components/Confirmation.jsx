5ad28fbea23414ee0e870edcf7f4833d8cf68019
Laura <laura.e.vollmer@gmail.com>
2024-09-02 20:08:58 -0500

    adding a new order confirmed section

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Confirmation = () => {
   return (
-    <div>Confirmation</div>
-  )
-}
+    <div>
+      <div>
+        <h1 className="font-redhat text-black font-bold text-4xl">
+          Order Confirmed
+        </h1>
+        <h2>We hope you enjoy your food!</h2>
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <button
+          onClick={handleQuantity}
+          className="absolute z-50 flex flex-row items-center justify-center py-2 px-6 space-x-2 bg-white rounded-full border border-rose-400"
+        >
+          <p className="font-bold text-rose-500">Start New Order</p>
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Confirmation
+export default Confirmation;
