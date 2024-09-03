fec34d2a18aa3f91d4076ae132df04ebb9ee937f
Laura <laura.e.vollmer@gmail.com>
2024-09-03 08:30:56 -0500

    styled the close icons

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -7,7 +7,9 @@ const Cart = ({ products, setQuantity, quantity }) => {
     <div className="bg-white rounded-lg p-10">
       <div className="flex flex-row space-x-1">
         <h1 className="font-redhat text-red font-bold text-xl">Your Cart</h1>
-        <h1 className="font-redhat text-red font-bold text-xl">({quantity})</h1>
+        <h1 className="font-redhat text-red font-bold text-xl">
+          ({setQuantity})
+        </h1>
       </div>
       <div>
         <img src={emptyCart} alt="Empty Cart" className="w-40 h-40" />
--- src/components/CartCard.jsx
+++ src/components/CartCard.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import CloseIcon from "../assets/icon-remove-item.svg";
 
 const CartCard = ({ title, price, quantity, setQuantity }) => {
+  const handleClick = () => {
+    setQuantity(0);
+  };
+
   return (
-    <div className="flex flex-col font-redhat bg-rose-50">
-      <div className="flex flex-col justify-start bg-rose-50 pt-10">
+    <div className="flex flex-col font-redhat bg-white p-2">
+      <div className="flex flex-col justify-start bg-white">
         <div className="flex flex-row justify-between">
           <h1 className="font-bold text-md">{title}</h1>
-          <img src={CloseIcon} alt="Close Icon" className="h-4 w-4" />
+          <button onClick={handleClick} className="flex items-center justify-center h-4 w-4 rounded-full border border-rose-300">
+            <img src={CloseIcon} alt="Close Icon" className="h-2 w-2" />
+          </button>
         </div>
         <div className="flex flex-row space-x-2">
           <p className="text-red font-bold">{quantity}x</p>
