b93ba7dddcd2fd894dafd7f2cf9cb8e6c4bf9f8a
Laura <laura.e.vollmer@gmail.com>
2024-09-03 08:41:34 -0500

    fixing the total quantity displaying

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,18 +3,24 @@ import emptyCart from "../assets/illustration-empty-cart.svg";
 import CartCard from "./CartCard";
 
 const Cart = ({ products, setQuantity, quantity }) => {
+  const totalQuantity = products.reduce((acc, product) => {
+    return acc + product.quantity;
+  }, 0);
+
   return (
     <div className="bg-white rounded-lg p-10">
       <div className="flex flex-row space-x-1">
         <h1 className="font-redhat text-red font-bold text-xl">Your Cart</h1>
         <h1 className="font-redhat text-red font-bold text-xl">
-          ({setQuantity})
+          ({totalQuantity})
         </h1>
-      </div>
-      <div>
-        <img src={emptyCart} alt="Empty Cart" className="w-40 h-40" />
-        <p>Your added items will appear here</p>
-      </div>
+        </div>
+      {totalQuantity === 0 ? (
+        <div>
+          <img src={emptyCart} alt="Empty Cart" className="w-40 h-40" />
+          <p>Your added items will appear here</p>
+        </div>
+      ) : (
       <div className="flex flex-col pt-10 ">
         <div>
           {products.map((product) => (
@@ -28,6 +34,7 @@ const Cart = ({ products, setQuantity, quantity }) => {
           ))}
         </div>
       </div>
+      )}
     </div>
   );
 };
--- src/components/CartCard.jsx
+++ src/components/CartCard.jsx
@@ -9,18 +9,12 @@ const CartCard = ({ title, price, quantity }) => {
 
   const total = `$${(parseFloat(price) * quantity).toFixed(2)}`;
 
-
-  const handleClick = () => {
-    setQuantity(0);
-  };
-
   return (
     <div className="flex flex-col font-redhat bg-white p-2">
       <div className="flex flex-col justify-start bg-white">
         <div className="flex flex-row justify-between">
           <h1 className="font-bold text-md">{title}</h1>
           <button
-            onClick={handleClick}
             className="flex items-center justify-center h-4 w-4 rounded-full border border-rose-300"
           >
             <img src={CloseIcon} alt="Close Icon" className="h-2 w-2" />
