e3dc57ba97bf5127e6a408a021bc0f55e146d55f
Laura <laura.e.vollmer@gmail.com>
2024-09-02 20:18:43 -0500

    styling the confirmation section

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import GreenIcon from "../assets/icon-order-confirmed.svg";
 
 const Confirmation = () => {
   return (
-    <div>
+    <div className="flex flex-col">
+      <img src={GreenIcon} alt="green checkmark" className="h-10 w-10" />
+      <h1 className="font-redhat text-black font-bold text-4xl">
+        Order Confirmed
+      </h1>
+      <h2 className="font-redhat text-gray text-lg">
+        We hope you enjoy your food!
+      </h2>
       <div>
-        <h1 className="font-redhat text-black font-bold text-4xl">
-          Order Confirmed
-        </h1>
-        <h2>We hope you enjoy your food!</h2>
-      </div>
-      <div className="flex flex-col justify-center items-center ">
-        <button className="absolute z-50 bg-red flex flex-row items-center justify-center py-2 px-6 space-x-2 rounded-full">
+        <button className="bg-red flex flex-row items-center justify-center py-2 px-4 rounded-full">
           <p className="font-bold text-rose-50 bg-red">Start New Order</p>
         </button>
       </div>
