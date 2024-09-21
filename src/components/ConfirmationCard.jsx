ed0abad8c5b8068c6c8e4b88105e068c46e50caa
Laura <laura.e.vollmer@gmail.com>
2024-09-21 08:46:33 -0500

    debugging confirmation card styling

--- src/components/ConfirmationCard.jsx
+++ src/components/ConfirmationCard.jsx
@@ -1,27 +1,40 @@
-import React from 'react'
+import React from "react";
 
-const ConfirmationCard = () => {
+const ConfirmationCard = ({ title, price, quantity }) => {
+  //component returns nothing if quantity is less than or equal to 0
+  if (quantity <= 0) {
+    return null;
+  }
+
+  console.log("Price:", price);
+  console.log("Quantity:", quantity);
+
+  // Remove dollar sign and ensure price is a valid number
+  const priceWithoutDollarSign = price.replace("$", "");
+  const parsedPrice = parseFloat(priceWithoutDollarSign);
+  console.log("Parsed Price:", parsedPrice);
+
+  const total = isNaN(parsedPrice)
+    ? "Invalid price"
+    : `$${(parsedPrice * quantity).toFixed(2)}`;
+  console.log("Total:", total);
   return (
-    <div className="flex flex-col font-redhat bg-white p-2">
-      <div className="flex flex-col justify-start bg-white">
+    <div className="flex flex-col font-redhat bg-rose-50 p-2">
+      <div className="flex flex-col justify-start">
         <div className="flex flex-row justify-between">
           <h1 className="font-bold text-md">{title}</h1>
-          <button
-            className="flex items-center justify-center h-4 w-4 rounded-full border border-rose-300"
-            onClick={removeItem}
-          >
-            <img src={CloseIcon} alt="Close Icon" className="h-2 w-2" />
-          </button>
         </div>
         <div className="flex flex-row space-x-2">
           <p className="text-red font-bold">{quantity}x</p>
-          <p className="text-rose-300 text-md">${isNaN(parsedPrice) ? "Invalid price" : parsedPrice.toFixed(2)}</p>
+          <p className="text-rose-300 text-md">
+            ${isNaN(parsedPrice) ? "Invalid price" : parsedPrice.toFixed(2)}
+          </p>
           <p className="text-red font-bold">{total}</p>
         </div>
         <hr className="border-rose-50 my-5" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ConfirmationCard
+export default ConfirmationCard;
